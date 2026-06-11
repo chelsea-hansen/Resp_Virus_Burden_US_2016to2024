ebc4811c9d66b9f484a62443f55ad7b1f9836f0e
Chelsea <c.hansen.mph@gmail.com>
2026-06-11 15:37:54 +0200

    script for running models

--- scripts/hospitalization_models.R
+++ scripts/hospitalization_models.R
@@ -17,12 +17,11 @@ source("R/model_functions.R")
 dat = readRDS("data/data_public.rds") 
 
 ages <- c("<1", "1to4", "5to49", "50to64", "65+")
-include_viruses = c("rsv_proxy_alternative","hmpv_proxy","rhino_proxy","covid_proxy")
-
+include_viruses = c("rsv_proxy_pediatric","hmpv_proxy","rhino_proxy","covid_proxy_alternative")
 
 # Respiratory Any Position ------------------------------------------------
 resp_any = bootstrap_block_residual(dat=dat,
-                                      y_value="hosp_resp_any_rate", 
+                                      y_value="hosp_resp_any_rate_smooth", 
                                       ages=ages, 
                                       lags = c(rep(0,5)),
                                       include_viruses_A = include_viruses,
@@ -33,11 +32,12 @@ resp_any = bootstrap_block_residual(dat=dat,
                                       block_length=6,
                                       n_boot = 1000) %>% 
   mutate(cause = 'Respiratory (Any Position)')
-saveRDS(resp_any,"model_output/hosp_resp_any.rds")
+saveRDS(resp_any,"main_results/hosp_resp_any.rds")
+
 
 # Respiratory First Position ----------------------------------------------
 resp_first = bootstrap_block_residual(dat=dat,
-                                      y_value="hosp_resp_1st_rate", 
+                                      y_value="hosp_resp_1st_rate_smooth", 
                                       ages=ages, 
                                       lags = c(rep(0,5)),
                                       include_viruses_A = include_viruses,
@@ -48,11 +48,11 @@ resp_first = bootstrap_block_residual(dat=dat,
                                       block_length=6,
                                       n_boot = 1000) %>% 
   mutate(cause = 'Respiratory (First Position)')
-saveRDS(resp_first,"model_output/hosp_resp_1st.rds")
+saveRDS(resp_first,"main_results/hosp_resp_1st.rds")
 
 # Respiratory or Circulatory First Position -------------------------------
 rc_first = bootstrap_block_residual(dat=dat,
-                                      y_value="hosp_respcirc_1st_rate", 
+                                      y_value="hosp_respcirc_1st_rate_smooth", 
                                       ages=ages, 
                                       lags = c(rep(0,5)),
                                       include_viruses_A = include_viruses,
@@ -63,5 +63,5 @@ rc_first = bootstrap_block_residual(dat=dat,
                                       block_length=6,
                                       n_boot = 1000) %>% 
   mutate(cause = 'Respiratory + Circulatory (First Position)')
-saveRDS(rc_first,"model_output/hosp_respcirc_1st.rds")
+saveRDS(rc_first,"main_results/hosp_respcirc_1st.rds")
 
--- scripts/mortality_models.R
+++ scripts/mortality_models.R
@@ -16,102 +16,57 @@ source("R/model_functions.R")
 dat = readRDS("data/data_public.rds") 
 
 ages <- c("<1", "1to4", "5to49", "50to64", "65+")
-ages_children <- c("<1", "1to4")
-ages_adults <- c("5to49", "50to64", "65+")
 
-
-include_viruses_children = c("rsv_proxy_alternative","hmpv_proxy","rhino_proxy","covid_proxy")
-include_viruses_adults = c("rsv_proxy_alternative","hmpv_proxy","covid_proxy")
-
+include_viruses = c("rsv_proxy_pediatric","hmpv_proxy","rhino_proxy","covid_proxy_alternative")
 
 
 # Respiratory Any Position ------------------------------------------------
-resp_mc_children = bootstrap_block_residual(dat=dat,
-                                    y_value="mort_resp_any_rate", 
-                                    ages=ages_children, 
-                                    lags = c(1,2,3,3,2),
-                                    include_viruses_A = include_viruses_children,
-                                    include_viruses_B = include_viruses_children,
-                                    include_viruses_C = include_viruses_children,
-                                    include_viruses_D = include_viruses_children,
-                                    include_viruses_E = include_viruses_children,
+resp_mc = bootstrap_block_residual(dat=dat,
+                                    y_value="mort_resp_any_rate_smooth", 
+                                    ages=ages, 
+                                    lags = c(1,2,2,3,2),
+                                    include_viruses_A = include_viruses,
+                                    include_viruses_B = include_viruses,
+                                    include_viruses_C = include_viruses,
+                                    include_viruses_D = include_viruses,
+                                    include_viruses_E = include_viruses,
                                     block_length=6,
                                     n_boot = 1000) %>% 
   mutate(cause = 'Respiratory (MC)')
 
-resp_mc_adults = bootstrap_block_residual(dat=dat,
-                                            y_value="mort_resp_any_rate", 
-                                            ages=ages_adults, 
-                                          lags = c(1,2,3,3,2),
-                                            include_viruses_A = include_viruses_adults,
-                                            include_viruses_B = include_viruses_adults,
-                                            include_viruses_C = include_viruses_adults,
-                                            include_viruses_D = include_viruses_adults,
-                                            include_viruses_E = include_viruses_adults,
-                                            block_length=6,
-                                            n_boot = 1000) %>% 
-  mutate(cause = 'Respiratory (MC)')
-
-resp_mc = rbind(resp_mc_children,resp_mc_adults)
-saveRDS(resp_mc,"model_output/mort_resp_any.rds")
+saveRDS(resp_mc,"main_results/mort_resp_any.rds")
 
 # Respiratory First Position ----------------------------------------------
-resp_uc_children = bootstrap_block_residual(dat=dat,
-                                      y_value="mort_resp_underlying_rate", 
-                                      ages=ages_children, 
-                                      lags = c(1,2,3,3,2),
-                                      include_viruses_A = include_viruses_children,
-                                      include_viruses_B = include_viruses_children,
-                                      include_viruses_C = include_viruses_children,
-                                      include_viruses_D = include_viruses_children,
-                                      include_viruses_E = include_viruses_children,
+resp_uc = bootstrap_block_residual(dat=dat,
+                                      y_value="mort_resp_underlying_rate_smooth", 
+                                      ages=ages, 
+                                      lags = c(1,2,2,3,2),
+                                      include_viruses_A = include_viruses,
+                                      include_viruses_B = include_viruses,
+                                      include_viruses_C = include_viruses,
+                                      include_viruses_D = include_viruses,
+                                      include_viruses_E = include_viruses,
                                       block_length=6,
                                       n_boot = 1000) %>% 
   mutate(cause = 'Respiratory (UC)')
 
-resp_uc_adults = bootstrap_block_residual(dat=dat,
-                                   y_value="mort_resp_underlying_rate", 
-                                   ages=ages_adults, 
-                                   lags = c(1,2,3,3,2),
-                                   include_viruses_A = include_viruses_adults,
-                                   include_viruses_B = include_viruses_adults,
-                                   include_viruses_C = include_viruses_adults,
-                                   include_viruses_D = include_viruses_adults,
-                                   include_viruses_E = include_viruses_adults,
-                                   block_length=6,
-                                   n_boot = 1000) %>% 
-  mutate(cause = 'Respiratory (UC)')
 
-resp_uc = rbind(resp_uc_children, resp_uc_adults)
-saveRDS(resp_uc,"model_output/mort_resp_underlying.rds")
+saveRDS(resp_uc,"main_results/mort_resp_underlying.rds")
 
 # Respiratory or Circulatory First Position -------------------------------
-rc_uc_children = bootstrap_block_residual(dat=dat,
-                                    y_value="mort_respcirc_underlying_rate", 
-                                    ages=ages_children, 
-                                    lags = c(1,2,3,3,2),
-                                    include_viruses_A = include_viruses_children,
-                                    include_viruses_B = include_viruses_children,
-                                    include_viruses_C = include_viruses_children,
-                                    include_viruses_D = include_viruses_children,
-                                    include_viruses_E = include_viruses_children,
+rc_uc= bootstrap_block_residual(dat=dat,
+                                    y_value="mort_respcirc_underlying_rate_smooth", 
+                                    ages=ages, 
+                                    lags = c(1,2,2,3,2),
+                                    include_viruses_A = include_viruses,
+                                    include_viruses_B = include_viruses,
+                                    include_viruses_C = include_viruses,
+                                    include_viruses_D = include_viruses,
+                                    include_viruses_E = include_viruses,
                                     block_length=6,
                                     n_boot = 1000) %>% 
   mutate(cause = 'Respiratory + Circulatory (UC)')
 
-rc_uc_adults = bootstrap_block_residual(dat=dat,
-                                          y_value="mort_respcirc_underlying_rate", 
-                                          ages=ages_adults, 
-                                          lags = c(1,2,3,3,2),
-                                          include_viruses_A = include_viruses_adults,
-                                          include_viruses_B = include_viruses_adults,
-                                          include_viruses_C = include_viruses_adults,
-                                          include_viruses_D = include_viruses_adults,
-                                          include_viruses_E = include_viruses_adults,
-                                          block_length=6,
-                                          n_boot = 1000) %>% 
-  mutate(cause = 'Respiratory + Circulatory (UC)')
-rc_uc = rbind(rc_uc_children, rc_uc_adults)
-saveRDS(rc_uc,"model_output/mort_respcirc_underlying.rds")
 
+saveRDS(rc_uc,"main_results/mort_respcirc_underlying.rds")
 
